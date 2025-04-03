d7ae50ea6fefeaed47632f4cff0f44c91d9729e1
kalvium <sanya.thakur@kalvium.community>
2025-04-03 11:07:34 +0530

    done

--- script.js
+++ script.js
@@ -15,9 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count= getCookie('count');
   // 2. If the cookie exists, increment the value and update the cookie
+  if(count!==null){
+    count=parseInt(count)+1;
+  }else{
   // 3. If the cookie does not exist, create it and set the value to 1
+  count=1;
+  }
   // 4. Display the count on the webpage
-
+  setCookie('count', count, 7);
+  document.getElementById('countDisplay').textContent=`You've visited this page ${count} times.`
   // your code here
 });
